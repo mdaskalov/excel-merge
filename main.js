1be90b144e79472e1b2c9a921800b439100e494a
Milko Daskalov <milko.daskalov@mpay24.com>
2019-08-01 18:22:35 +0200

    added button functionality

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 // Modules to control application life and create native browser window
 const {
   app,
-  BrowserWindow,
-  Menu,
-  shell,
-  dialog
+  ipcMain,
+  dialog,
+  BrowserWindow
 } = require('electron')
 const path = require('path')
 
@@ -12,272 +11,21 @@ const path = require('path')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow = null
 
-
-
-
-let template = [{
-    label: 'File',
-    submenu: [{
-      label: 'Open File',
-    }, {
-      label: 'Save',
-      accelerator: 'CmdOrCtrl+S',
-      role: 'redo'
-    }, {
-      type: 'separator'
-    }, {
-      label: 'Save as...',
-      accelerator: 'Shift+CmdOrCtrl+S',
-      role: 'cut'
-    }]
-  },
-
-  {
-    label: 'Edit',
-    submenu: [{
-      label: 'Undo',
-      accelerator: 'CmdOrCtrl+Z',
-      role: 'undo'
-    }, {
-      label: 'Redo',
-      accelerator: 'Shift+CmdOrCtrl+Z',
-      role: 'redo'
-    }, {
-      type: 'separator'
-    }, {
-      label: 'Cut',
-      accelerator: 'CmdOrCtrl+X',
-      role: 'cut'
-    }, {
-      label: 'Copy',
-      accelerator: 'CmdOrCtrl+C',
-      role: 'copy'
-    }, {
-      label: 'Paste',
-      accelerator: 'CmdOrCtrl+V',
-      role: 'paste'
-    }, {
-      label: 'Select All',
-      accelerator: 'CmdOrCtrl+A',
-      role: 'selectall'
-    }]
-  }, {
-    label: 'View',
-    submenu: [{
-      label: 'Reload',
-      accelerator: 'CmdOrCtrl+R',
-      click: (item, focusedWindow) => {
-        if (focusedWindow) {
-          // on reload, start fresh and close any old
-          // open secondary windows
-          if (focusedWindow.id === 1) {
-            BrowserWindow.getAllWindows().forEach(win => {
-              if (win.id > 1) win.close()
-            })
-          }
-          focusedWindow.reload()
-        }
-      }
-    }, {
-      label: 'Toggle Full Screen',
-      accelerator: (() => {
-        if (process.platform === 'darwin') {
-          return 'Ctrl+Command+F'
-        } else {
-          return 'F11'
-        }
-      })(),
-      click: (item, focusedWindow) => {
-        if (focusedWindow) {
-          focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
-        }
-      }
-    }, {
-      label: 'Toggle Developer Tools',
-      accelerator: (() => {
-        if (process.platform === 'darwin') {
-          return 'Alt+Command+I'
-        } else {
-          return 'Ctrl+Shift+I'
-        }
-      })(),
-      click: (item, focusedWindow) => {
-        if (focusedWindow) {
-          focusedWindow.toggleDevTools()
-        }
-      }
-    }, {
-      type: 'separator'
-    }, {
-      label: 'App Menu Demo',
-      click: function(item, focusedWindow) {
-        if (focusedWindow) {
-          const options = {
-            type: 'info',
-            title: 'Application Menu Demo',
-            buttons: ['Ok'],
-            message: 'This demo is for the Menu section, showing how to create a clickable menu item in the application menu.'
-          }
-          dialog.showMessageBox(focusedWindow, options, function() {})
-        }
-      }
-    }]
-  }, {
-    label: 'Window',
-    role: 'window',
-    submenu: [{
-      label: 'Minimize',
-      accelerator: 'CmdOrCtrl+M',
-      role: 'minimize'
-    }, {
-      label: 'Close',
-      accelerator: 'CmdOrCtrl+W',
-      role: 'close'
-    }, {
-      type: 'separator'
-    }, {
-      label: 'Reopen Window',
-      accelerator: 'CmdOrCtrl+Shift+T',
-      enabled: false,
-      key: 'reopenMenuItem',
-      click: () => {
-        app.emit('activate')
-      }
-    }]
-  }, {
-    label: 'Help',
-    role: 'help',
-    submenu: [{
-      label: 'Learn More',
-      click: () => {
-        shell.openExternal('http://electron.atom.io')
-      }
-    }]
-  }
-]
-
-function addUpdateMenuItems(items, position) {
-  if (process.mas) return
-
-  const version = app.getVersion()
-  let updateItems = [{
-    label: `Version ${version}`,
-    enabled: false
-  }, {
-    label: 'Checking for Update',
-    enabled: false,
-    key: 'checkingForUpdate'
-  }, {
-    label: 'Check for Update',
-    visible: false,
-    key: 'checkForUpdate',
-    click: () => {
-      require('electron').autoUpdater.checkForUpdates()
-    }
-  }, {
-    label: 'Restart and Install Update',
-    enabled: true,
-    visible: false,
-    key: 'restartToUpdate',
-    click: () => {
-      require('electron').autoUpdater.quitAndInstall()
-    }
-  }]
-
-  items.splice.apply(items, [position, 0].concat(updateItems))
-}
-
-function findReopenMenuItem() {
-  const menu = Menu.getApplicationMenu()
-  if (!menu) return
-
-  let reopenMenuItem
-  menu.items.forEach(item => {
-    if (item.submenu) {
-      item.submenu.items.forEach(item => {
-        if (item.key === 'reopenMenuItem') {
-          reopenMenuItem = item
-        }
-      })
-    }
-  })
-  return reopenMenuItem
-}
-
-if (process.platform === 'darwin') {
-  const name = app.getName()
-  template.unshift({
-    label: 'Excel Merger',
-    submenu: [{
-      label: `About Excel Merger`,
-      role: 'about'
-    }, {
-      type: 'separator'
-    }, {
-      label: 'Services',
-      role: 'services',
-      submenu: []
-    }, {
-      type: 'separator'
-    }, {
-      label: `Hide ${name}`,
-      accelerator: 'Command+H',
-      role: 'hide'
-    }, {
-      label: 'Hide Others',
-      accelerator: 'Command+Alt+H',
-      role: 'hideothers'
-    }, {
-      label: 'Show All',
-      role: 'unhide'
-    }, {
-      type: 'separator'
-    }, {
-      label: 'Quit',
-      accelerator: 'Command+Q',
-      click: () => {
-        app.quit()
-      }
-    }]
-  })
-
-  // Window menu.
-  template[3].submenu.push({
-    type: 'separator'
-  }, {
-    label: 'Bring All to Front',
-    role: 'front'
-  })
-
-  addUpdateMenuItems(template[0].submenu, 1)
-}
-
-if (process.platform === 'win32') {
-  const helpMenu = template[template.length - 1].submenu
-  addUpdateMenuItems(helpMenu, 0)
-}
-
-
-
-
-
-
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    'min-width': 500,
-    'min-height': 200,
-    'accept-first-mouse': true,
-    'title-bar-style': 'hidden',
+    minWidth: 800,
+    minHeight: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'js/menu.js')
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile(path.join(__dirname, 'index.html'))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -289,28 +37,15 @@ function createWindow() {
     // when you should delete the corresponding element.
     mainWindow = null
   })
-
-  const menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(menu)
 }
-
-
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-app.on('browser-window-created', () => {
-  let reopenMenuItem = findReopenMenuItem()
-  if (reopenMenuItem) reopenMenuItem.enabled = false
-})
-
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  let reopenMenuItem = findReopenMenuItem()
-  if (reopenMenuItem) reopenMenuItem.enabled = true
-
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
@@ -322,5 +57,31 @@ app.on('activate', function() {
   if (mainWindow === null) createWindow()
 })
 
+const sourceParser = require('./src/source-parser')
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+ipcMain.on('open-file-dialog', event => {
+  dialog.showOpenDialog({
+    properties: ['openFile']
+  }, files => {
+    if (files) {
+      var relativePath = path.relative(process.cwd(), files[0]);
+      console.log(relativePath)
+      sourceParser.parseSourceFile(relativePath, data => {
+        dumpData(data)
+        event.sender.send('parsed-data', data)
+      })
+    }
+  })
+})
+
+
+const dumpData = data => {
+  data.forEach(dat => {
+    console.log(`${dat.stairway} / ${dat.floor} / Top: ${dat.apt}, Einheit: ${dat.unit}`)
+    dat.content.forEach(cnt => {
+      console.log(`  ${cnt.room} - ${cnt.surface} m2`)
+    })
+  })
+}
--- renderer.js
+++ renderer.js
@@ -1,3 +1,22 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const {
+  ipcRenderer
+} = require('electron')
+
+document.querySelector('#open-source-file').addEventListener('click', () => {
+  ipcRenderer.send('open-file-dialog')
+})
+
+ipcRenderer.on('parsed-data', (event, data) => {
+  const tbody = document.querySelector("#table-body")
+  var html = ""
+  data.forEach(dat => {
+    html += `<tr><td class="group-header" colspan="2">${dat.stairway} / ${dat.floor} / Top: ${dat.apt}, Einheit: ${dat.unit}</td></tr>`
+    dat.content.forEach(cnt => {
+      html += `<tr><td>${cnt.room}</td><td>${cnt.surface}</td></tr>`
+    })
+  })
+  tbody.innerHTML = html;
+})
