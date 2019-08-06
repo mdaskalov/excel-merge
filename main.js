c54b92ed2c4ef01e03d37e5f010afc3d77d7b89b
Milko Daskalov <milko.daskalov@mpay24.com>
2019-08-06 17:10:09 +0200

    moved to the renderer

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const {
   BrowserWindow
 } = require('electron')
 const path = require('path')
-require('./communication')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
